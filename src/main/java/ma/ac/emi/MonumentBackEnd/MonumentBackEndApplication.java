b332f095ce918bfb8dd5a229083f3f2bddb17c60
ChihabMakkoudi <chihabmakkoudi.cpge@gmail.com>
2022-04-09 13:43:58 +0000

    init

---
+++ src/main/java/ma/ac/emi/MonumentBackEnd/MonumentBackEndApplication.java
@@ -0,0 +1,13 @@
+package ma.ac.emi.MonumentBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonumentBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonumentBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/ac/emi/MonumentBackEnd/MonumentBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ac.emi.MonumentBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonumentBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
