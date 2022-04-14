5b5fb80e83fe2ed77b7dc47d56c2b7689e3973f7
The7thHeart <elherzahmedonline@gmail.com>
2022-04-14 12:36:34 +0000

    coded the implimentation of IDBUserAdder

--- src/main/java/ma/ac/emi/MonumentBackEnd/DAO/UserDAO.java
+++ src/main/java/ma/ac/emi/MonumentBackEnd/DAO/UserDAO.java
@@ -1,5 +1,21 @@
 package ma.ac.emi.MonumentBackEnd.DAO;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import java.io.File;
+
+
 import ma.ac.emi.MonumentBackEnd.Entities.Utilisateur;
 import ma.ac.emi.MonumentBackEnd.UserControllerpackage.IDBUserAdder;
 import ma.ac.emi.MonumentBackEnd.UserControllerpackage.IDBUserDeletter;
@@ -27,8 +43,57 @@ public class UserDAO implements IDBUserAdder,IDBUserGetter,IDBUserDeletter{
 
     @Override
     public void addUtilisateur(Utilisateur utilisateur) {
-        // TODO Auto-generated method stub
+
+        try {
+        DocumentBuilderFactory dbFactory =
+        DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder;
+            dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.newDocument();
         
+        // root element
+        Element rootElement = doc.createElement("Account");
+        doc.appendChild(rootElement);
+        
+        // setting id attribute to account
+        Attr attr = doc.createAttribute("id");
+        attr.setValue(utilisateur.getId());
+        rootElement.setAttributeNode(attr);
+
+        // nom element
+        Element nom = doc.createElement("nom");
+        nom.appendChild(doc.createTextNode(utilisateur.getNom()));
+        rootElement.appendChild(nom);
+
+        // nom element
+        Element prenom = doc.createElement("prenom");
+        prenom.appendChild(doc.createTextNode(utilisateur.getPrenom()));
+        rootElement.appendChild(prenom);
+
+        // email element
+        Element email = doc.createElement("email");
+        email.appendChild(doc.createTextNode(utilisateur.getMail()));
+        rootElement.appendChild(email);
+
+        // password element
+        Element password = doc.createElement("password");
+        password.appendChild(doc.createTextNode(utilisateur.getPasseword()));
+        rootElement.appendChild(password);
+
+        // write the content into xml file
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(new File("/users/"+utilisateur.getMail()+".xml"));
+        transformer.transform(source, result);
+        
+        // Output to console for testing
+        StreamResult consoleResult = new StreamResult(System.out);
+        transformer.transform(source, consoleResult);
+        
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
 }
