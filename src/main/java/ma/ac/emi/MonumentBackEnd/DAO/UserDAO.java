c52fdfb35156b68f862e195d72c7e6d4a16f5edf
The7thHeart <elherzahmedonline@gmail.com>
2022-04-16 15:39:52 +0000

    added getting user by mail and password

--- src/main/java/ma/ac/emi/MonumentBackEnd/DAO/UserDAO.java
+++ src/main/java/ma/ac/emi/MonumentBackEnd/DAO/UserDAO.java
@@ -64,9 +64,34 @@ public class UserDAO implements IDBUserAdder,IDBUserGetter,IDBUserDeletter{
         return null;
     }
 
+    // get user if mail and password are good 
+    // return null otherwise
     @Override
     public Utilisateur getUtilisateur(String mail, String passeword) {
-        // TODO Auto-generated method stub
+        File dir = new File("users/");
+        File[] files = dir.listFiles((dir1, name) -> name.endsWith(mail+".xml"));
+        if (files.length<1) return null;
+        File inputFile = files[0];
+        try {
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(inputFile);
+        doc.getDocumentElement().normalize();
+
+        String id = doc.getDocumentElement().getAttribute("id");
+        String nom = doc.getDocumentElement().getElementsByTagName("nom").item(0).getTextContent();
+        String prenom = doc.getDocumentElement().getElementsByTagName("prenom").item(0).getTextContent();
+        String result_password = doc.getDocumentElement().getElementsByTagName("password").item(0).getTextContent();
+        if (!passeword.equals(result_password)) return null;
+        
+        return new Utilisateur(id, nom, prenom, mail, passeword);
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
         return null;
     }
 
