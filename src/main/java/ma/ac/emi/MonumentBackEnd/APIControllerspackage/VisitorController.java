4365a9cc0c40756dda5193bb413753c9bd8a2e81
The7thHeart <elherzahmedonline@gmail.com>
2022-05-11 23:51:54 +0100

    authentification POST

--- src/main/java/ma/ac/emi/MonumentBackEnd/APIControllerspackage/VisitorController.java
+++ src/main/java/ma/ac/emi/MonumentBackEnd/APIControllerspackage/VisitorController.java
@@ -1,20 +1,27 @@
 package ma.ac.emi.MonumentBackEnd.APIControllerspackage;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.ctc.wstx.shaded.msv_core.util.Util;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ma.ac.emi.MonumentBackEnd.Entities.Monument;
+import ma.ac.emi.MonumentBackEnd.Entities.Utilisateur;
 
 @RestController
 public class VisitorController {
     
     @Autowired
     private IMonumentGetter monumentGetter;
+    @Autowired
+    private IConnectionController connectionController;
 
     @GetMapping("/monument")
     public Monument getMonument(@RequestParam String id) {
@@ -30,5 +37,12 @@ public class VisitorController {
         return monumentGetter.getMonuments(motClets);
     }
     
+    @PostMapping("/auth")
+    public String authentification(@RequestParam String email, @RequestParam String password) throws NoSuchAlgorithmException {
+        Utilisateur user = connectionController.connection(email, password);
+        if (user == null)
+            return "fail";
+        return user.getToken();
+    }
 
 }
