cbddf1c1e791b7566235190ad271b8bc3fed0b28
The7thHeart <elherzahmedonline@gmail.com>
2022-05-12 00:27:58 +0100

    evaluation deleting

--- src/main/java/ma/ac/emi/MonumentBackEnd/APIControllerspackage/AdminController.java
+++ src/main/java/ma/ac/emi/MonumentBackEnd/APIControllerspackage/AdminController.java
@@ -5,16 +5,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import ma.ac.emi.MonumentBackEnd.MonumentControllerpackage.IDBEvaluationDeletter;
+
 @RestController
 public class AdminController {
 
     @Autowired
     private IMonumentDeleter monumentDeleter;
+    @Autowired
+    private IDBEvaluationDeletter evaluationDeletter;
 
     @PostMapping("/monument/delete")
     public void deleteMonument(@RequestParam String monumentId) {
         monumentDeleter.deleteMonument(monumentId);
-
     }
-    
+    @PostMapping("/evaluation/delete")
+    public void deleteEvaluation(@RequestParam String evaluationId) {
+        evaluationDeletter.deleteEvaluation(evaluationId);
+    }
 }
